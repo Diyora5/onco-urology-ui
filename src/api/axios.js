609ebcd3f85cf296955224cbd4c6b46024209aaa
Diyora05 <diyora_dadadjanova@student.itpu.uz>
2026-05-31 15:16:09 +0500

    new version

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-  import.meta.env.VITE_API_BASE_URL || 'https://doctor-api.onrender.com/api';
+  import.meta.env.VITE_API_BASE_URL || 'https://olco-urology-api.vercel.app/api';
 
 const api = axios.create({
   baseURL,
