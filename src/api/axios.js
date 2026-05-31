ed3d3138f81ca4622b7b7fae1419e0a20f5f6274
Diyora05 <diyora_dadadjanova@student.itpu.uz>
2026-05-31 15:21:19 +0500

    new ff

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-  import.meta.env.VITE_API_BASE_URL || 'https://olco-urology-api.vercel.app/api';
+  import.meta.env.VITE_API_BASE_URL || 'https://olco-urology-faly90wzj-cocoshs-projects.vercel.app/api';
 
 const api = axios.create({
   baseURL,
